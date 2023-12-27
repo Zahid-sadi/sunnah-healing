452d2c754f116e4620fa57a1fe3bd8f1e38cd771
Zahid-saadi <zahidbinkashem@gmail.com>
2023-12-27 06:23:31 +0600

    style correction on about section

--- src/Pages/Home/About/About.jsx
+++ src/Pages/Home/About/About.jsx
@@ -5,9 +5,9 @@ const About = () => {
     return (
         <div className="hero mt-10 h-fit p-10 bg-sky-300">
             <div className="hero-content flex-col lg:flex-row-reverse  rounded-lg ">
-                <div data-aos="fade-down-left" className="   bg-blue-400 rounded-lg shadow-2xl w-1/2 p-8 text-start font-serif">
+                <div data-aos="fade-down-left" className="   bg-blue-400 rounded-lg shadow-xl shadow-sky-500 w-1/2 p-8 text-start  font-serif">
                   <img className="overlay-50 h-fit" src={img} alt="" />
-                  <h1 className="text-4xl font-extrabold p-5 text-center ">About Us</h1>
+                  <h1 className="text-4xl font-extrabold p-5 text-center text-white">About Us</h1>
                     <p>Our team of highly trained professionals incorporates the latest complementary therapies into their
                     practice to bring about effective healing and a renewed sense of vitality. Whether you are seeking
                     treatment for acute or chronic health conditions, or just want to maintain your health at its peak,
@@ -15,16 +15,16 @@ const About = () => {
                     At Hijama Singapore, we take a holistic approach to well-being by providing a comprehensive range of
                     .</p>
                 </div>
-                <div  data-aos="fade-down-right" className="w-1/2 shadow-2xl p-10 rounded-lg bg-gradient-to-r from-blue-300 to-blue-500 ... font-serif text-black ">
+                <div  data-aos="fade-down-right" className="w-1/2 shadow-2xl shadow-sky-400 p-10 rounded-lg bg-gradient-to-r from-sky-400 to-blue-400 ... font-serif text-black ">
                     
-                    <h2 className="text-4xl font-extrabold p-5 text-center text-green-800">How we care </h2>
-                    <h4 className="text-xl font-bold text-green-700">Certified practitioner</h4>
+                    <h2 className="text-4xl font-extrabold p-5 text-center text-white">How we care </h2>
+                    <h4 className="text-xl font-bold text-blue-800">Certified practitioner</h4>
                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus, consequuntur.</p>
-                    <h4 className="text-xl font-bold text-green-700 ">Modern technology</h4>
+                    <h4 className="text-xl font-bold text-blue-800 ">Modern technology</h4>
                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus, consequuntur.</p>
-                    <h4  className="text-xl font-bold text-green-700 ">House Call Service</h4>
+                    <h4  className="text-xl font-bold text-blue-800 ">House Call Service</h4>
                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus, consequuntur.</p>
-                    <h4  className="text-xl font-bold text-green-700">Membership / Year</h4>
+                    <h4  className="text-xl font-bold text-blue-800">Membership / Year</h4>
                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum, qui?</p>
 
                 </div>
