d3db9372562db2f8b5318084beff2d1abd464d8a
Zahid-saadi <zahidbinkashem@gmail.com>
2023-12-27 06:11:20 +0600

    faq-section-style-correction

--- src/Pages/Home/Faq/Faq.jsx
+++ src/Pages/Home/Faq/Faq.jsx
@@ -3,37 +3,37 @@ import { Accordion, AccordionBody, AccordionHeader, AccordionItem } from "react-
 
 const Faq = () => {
     return (
-        <div className="hero h-fit lg:h-[450px] bg--300 p-10  mx-auto">
-            <div className="hero-content flex-col lg:flex-row-reverse mx-auto ">
+        <div className=" h-fit w-full lg:h-[450px] bg-sky-300 py-10  ">
+            <div className="hero-content w-full flex-col lg:flex-row-reverse mx-auto ">
                 
                 <div className="text-center h-[450px]  lg:text-left">
-                    <h1 className=" text-4xl p-5 bg-blue-400 rounded-lg shadow-2xl mb-5  font-bold text-center  shadow-blue-600 ">Massage</h1>
+                    <h1 className=" text-4xl p-5 bg-blue-400 rounded-lg shadow-2xl mb-5  font-bold text-center  shadow-blue-700 ">Massage</h1>
                     <Accordion>
                     <AccordionItem>
-                            <AccordionHeader className= " bg-blue-300 p-4 rounded-t-xl mb-2 w-full font-bold text-lg shadow-xl shadow-blue-600">
-                                <h3 className={`accordion-title`}></h3>Massage kokhon korar boyos koto
+                            <AccordionHeader className= " bg-sky-300 p-4 rounded-t-xl mb-2 w-full font-bold shadow-xl shadow-sky-500 hover:bg-sky-400 hover:shadow-sm hover:shadow-sky-600 scale-100 duration-700  ">
+                                <h3 className={`accordion-title`}></h3>Massage korar boyos koto
                             </AccordionHeader>
 
                             <AccordionBody>
-                                <div className="accordion-body rounded-b-xl bg-blue-100 p-4 mb-2">apni je kono boyosei hijama korte parben .  </div>
+                                <div className="accordion-body rounded-b-xl bg-sky-200 p-4 mb-2">apni je kono boyosei hijama korte parben .  </div>
                             </AccordionBody>
                         </AccordionItem>
                     <AccordionItem>
-                            <AccordionHeader className= " bg-blue-300 p-4 rounded-t-xl mb-2 w-full font-bold text-lg shadow-xl shadow-blue-600">
+                            <AccordionHeader className= " bg-sky-300 p-4 rounded-t-xl mb-2 w-full font-bold shadow-xl shadow-sky-500 hover:bg-sky-400 hover:shadow-sm hover:shadow-sky-600 scale-100 duration-700  ">
                                 <h3 className={`accordion-title`}></h3>Massage kokhon korar boyos koto
                             </AccordionHeader>
 
                             <AccordionBody>
-                                <div className="accordion-body rounded-b-xl bg-blue-100 p-4 mb-2">apni je kono boyosei hijama korte parben .  </div>
+                                <div className="accordion-body rounded-b-xl bg-sky-200 p-4 mb-2">apni je kono boyosei hijama korte parben .  </div>
                             </AccordionBody>
                         </AccordionItem>
                     <AccordionItem>
-                            <AccordionHeader className= " bg-blue-300 p-4 rounded-t-xl mb-2 w-full font-bold text-lg shadow-xl shadow-blue-600">
+                            <AccordionHeader className= " bg-sky-300 p-4 rounded-t-xl mb-2 w-full font-bold shadow-xl shadow-sky-500 hover:bg-sky-400 hover:shadow-sm hover:shadow-sky-600 scale-100 duration-700  ">
                                 <h3 className={`accordion-title`}></h3>Massage kokhon korar boyos koto
                             </AccordionHeader>
 
                             <AccordionBody>
-                                <div className="accordion-body rounded-b-xl bg-blue-100 p-4 mb-2">apni je kono boyosei hijama korte parben .  </div>
+                                <div className="accordion-body rounded-b-xl bg-sky-200 p-4 mb-2">apni je kono boyosei hijama korte parben .  </div>
                             </AccordionBody>
                         </AccordionItem>
 
@@ -41,33 +41,33 @@ const Faq = () => {
                     </Accordion>
                 </div>
                 <div className="text-center h-[450px] lg:text-left">
-                    <h1 className=" text-4xl p-5 bg-blue-400 rounded-lg shadow-2xl mb-5  font-bold text-center  shadow-blue-600 ">Ruqiya</h1>
+                    <h1 className=" text-4xl p-5 bg-blue-400 rounded-lg shadow-2xl mb-5  font-bold text-center  shadow-blue-700 ">Ruqiya</h1>
                     <Accordion>
                     <AccordionItem>
-                            <AccordionHeader className= " bg-blue-300 p-4 rounded-t-xl mb-2 w-full font-bold text-lg shadow-xl shadow-blue-600">
+                            <AccordionHeader className= " bg-sky-300 p-4 rounded-t-xl mb-2 w-full font-bold shadow-xl shadow-sky-500 hover:bg-sky-400 hover:shadow-sm hover:shadow-sky-600 scale-100 duration-700  ">
                                 <h3 className={`accordion-title`}></h3>Ruqiya kokhon korar boyos koto
                             </AccordionHeader>
 
                             <AccordionBody>
-                                <div className="accordion-body rounded-b-xl bg-blue-100 p-4 mb-2">apni je kono boyosei hijama korte parben .  </div>
+                                <div className="accordion-body rounded-b-xl bg-sky-200 p-4 mb-2">apni je kono boyosei hijama korte parben .  </div>
                             </AccordionBody>
                         </AccordionItem>
                     <AccordionItem>
-                            <AccordionHeader className= " bg-blue-300 p-4 rounded-t-xl mb-2 w-full font-bold text-lg shadow-xl shadow-blue-600">
+                            <AccordionHeader className= " bg-sky-300 p-4 rounded-t-xl mb-2 w-full font-bold shadow-xl shadow-sky-500 hover:bg-sky-400 hover:shadow-sm hover:shadow-sky-600 scale-100 duration-700  ">
                                 <h3 className={`accordion-title`}></h3>Ruqiya kokhon korar boyos koto
                             </AccordionHeader>
 
                             <AccordionBody>
-                                <div className="accordion-body rounded-b-xl bg-blue-100 p-4 mb-2">apni je kono boyosei hijama korte parben .  </div>
+                                <div className="accordion-body rounded-b-xl bg-sky-200 p-4 mb-2">apni je kono boyosei hijama korte parben .  </div>
                             </AccordionBody>
                         </AccordionItem>
                     <AccordionItem>
-                            <AccordionHeader className= " bg-blue-300 p-4 rounded-t-xl mb-2 w-full font-bold text-lg shadow-xl shadow-blue-600">
+                            <AccordionHeader className= " bg-sky-300 p-4 rounded-t-xl mb-2 w-full font-bold shadow-xl shadow-sky-500 hover:bg-sky-400 hover:shadow-sm hover:shadow-sky-600 scale-100 duration-700  ">
                                 <h3 className={`accordion-title`}></h3>Ruqiya kokhon korar boyos koto
                             </AccordionHeader>
 
                             <AccordionBody>
-                                <div className="accordion-body rounded-b-xl bg-blue-100 p-4 mb-2">apni je kono boyosei hijama korte parben .  </div>
+                                <div className="accordion-body rounded-b-xl bg-sky-200 p-4 mb-2">apni je kono boyosei hijama korte parben .  </div>
                             </AccordionBody>
                         </AccordionItem>
 
@@ -75,33 +75,33 @@ const Faq = () => {
                     </Accordion>
                 </div>
                 <div className="text-center h-[450px] lg:text-left">
-                    <h1 className=" text-4xl p-5 bg-blue-400 rounded-lg shadow-2xl mb-5  font-bold text-center  shadow-blue-600 ">Hijama</h1>
+                    <h1 className=" text-4xl p-5 bg-blue-400 rounded-lg shadow-2xl mb-5  font-bold text-center  shadow-blue-700 ">Hijama</h1>
                     <Accordion>
                     <AccordionItem>
-                            <AccordionHeader className= " bg-blue-300 p-4 rounded-t-xl mb-2 w-full font-bold text-lg shadow-xl shadow-blue-600">
+                            <AccordionHeader className= " bg-sky-300 p-4 rounded-t-xl mb-2 w-full font-bold shadow-xl shadow-sky-500 hover:bg-sky-400 hover:shadow-sm hover:shadow-sky-600 scale-100 duration-700  ">
                                 <h3 className={`accordion-title`}></h3>hijama kokhon korar boyos koto
                             </AccordionHeader>
 
                             <AccordionBody>
-                                <div className="accordion-body rounded-b-xl bg-blue-100 p-4 mb-2">apni je kono boyosei hijama korte parben .  </div>
+                                <div className="accordion-body rounded-b-xl bg-sky-200 p-4 mb-2">apni je kono boyosei hijama korte parben .  </div>
                             </AccordionBody>
                         </AccordionItem>
                     <AccordionItem>
-                            <AccordionHeader className= " bg-blue-300 p-4 rounded-t-xl mb-2 w-full font-bold text-lg shadow-xl shadow-blue-600">
+                            <AccordionHeader className= " bg-sky-300 p-4 rounded-t-xl mb-2 w-full font-bold shadow-xl shadow-sky-500 hover:bg-sky-400 hover:shadow-sm hover:shadow-sky-600 scale-100 duration-700  ">
                                 <h3 className={`accordion-title`}></h3>hijama kokhon korar boyos koto
                             </AccordionHeader>
 
                             <AccordionBody>
-                                <div className="accordion-body rounded-b-xl bg-blue-100 p-4 mb-2">apni je kono boyosei hijama korte parben .  </div>
+                                <div className="accordion-body rounded-b-xl bg-sky-200 p-4 mb-2">apni je kono boyosei hijama korte parben .  </div>
                             </AccordionBody>
                         </AccordionItem>
                     <AccordionItem>
-                            <AccordionHeader className= " bg-blue-300 p-4 rounded-t-xl mb-2 w-full font-bold text-lg shadow-xl shadow-blue-600">
+                            <AccordionHeader className= " bg-sky-300 p-4 rounded-t-xl mb-2 w-full font-bold shadow-xl shadow-sky-500 hover:bg-sky-400 hover:shadow-sm hover:shadow-sky-600 scale-100 duration-700  ">
                                 <h3 className={`accordion-title`}></h3>hijama kokhon korar boyos koto
                             </AccordionHeader>
 
                             <AccordionBody>
-                                <div className="accordion-body rounded-b-xl bg-blue-100 p-4 mb-2">apni je kono boyosei hijama korte parben .  </div>
+                                <div className="accordion-body rounded-b-xl bg-sky-200 p-4 mb-2">apni je kono boyosei hijama korte parben .  </div>
                             </AccordionBody>
                         </AccordionItem>
 
